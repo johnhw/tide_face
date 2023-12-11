0ca9ad3273cfb6b0e27236ee68aa793996e08c68
John <johnhw@gmail.com>
2023-12-11 20:01:04 +0000

    Initial commit with messy debug

--- extract_tides.py
+++ extract_tides.py
@@ -2,77 +2,6 @@ import json, os, math
 import click, time 
 
 
-def predict_tide_debug2(t, constituents, station):
-    """Predict the tide at a given time t, using the constituents
-    and station data"""
-    tide = 0
-    # compute the year this is in, and get seconds since the start of the year
-    # note: times are in UTC
-    for s0 in [-1, 0, 1]:
-        year = time.gmtime(t)[0]
-        t = t - epoch(year+s0)    
-        hour, min = unpack_tz(station["zone_offset"])    
-        t = t + ( hour * 3600 + min * 60) # NB check signs!    
-        for s1 in [-1, 1]:
-            for s2 in [-1, 1]:
-                for s3 in [-1, 1]:
-                    for s4 in [0, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75]:                    
-                        tide = 0                    
-                        for k, v in constituents.items():
-                            if k in station["constituents"]:
-                                year_constituents = v["years"][str(year)]
-                                constituent = station["constituents"][k]
-                                amp = constituent["amp"] * year_constituents["node_factor"] 
-                                phase = math.radians(s1*constituent["phase"] + s2* year_constituents["equilibrium"]) # nb check signs!
-                                speed = rads_per_second(v["speed"])
-                                tide += amp * math.cos(s3*speed * t + phase + math.pi*s4)
-                        print(f"{s0}\t{s1}\t{s2}\t{s3}\t{s4}\t{tide+station['offset']}")
-    return tide + station["offset"]
-
-def predict_tide_debug(t, constituents, station):
-    """Predict the tide at a given time t, using the constituents
-    and station data"""
-    def unpack_tz(tz):
-        return tz//100, tz%100
-
-    def epoch(year):
-        """Return the Unix time at the start of the year"""
-        return time.mktime((year, 1, 1, 0, 0, 0, 0, 0, 0))
-
-    def rads_per_second(degrees_per_hour):
-        return math.radians(degrees_per_hour/3600)
-    tide = 0
-    # compute the year this is in, and get seconds since the start of the year
-    # note: times are in UTC
-    print(f"Start time: {t}")
-    
-    year = time.gmtime(t)[0]
-    t = t - epoch(year)    
-    hour, min = unpack_tz(station["zone_offset"])    
-    phase_add = ( hour * 3600 + min * 60) # NB check signs!    
-    print(f"Epoch: {epoch(year)}, Year: {year}")
-    print(f"Phase add: {phase_add}")
-    print(f"Since epoch: {t}")    
-    print(f"Datum offset: {station['offset']}")
-
-    
-    tide = 0                    
-    for k, v in constituents.items():
-        if k in station["constituents"]:
-            year_constituents = v["years"][str(year)]
-            constituent = station["constituents"][k]
-            amp = constituent["amp"] * year_constituents["node_factor"]
-            phase = -(math.radians(constituent["phase"]) - phase_add * rads_per_second(v["speed"]))
-            adj_phase = phase + math.radians(year_constituents["equilibrium"])
-            if amp > 0:
-                print(f"{k}\tamp: {constituent['amp']:4.5f}\tphase: {phase:4.3f}\tspeed:{rads_per_second(v['speed'])*10000:4.3f}\tNod: {year_constituents['node_factor']:4.6f}\tequilib {math.radians(year_constituents['equilibrium']):4.6f}\tadj_phase: {adj_phase:4.6f}") 
-            
-            speed = rads_per_second(v["speed"])
-            tide += amp * math.cos(speed * t + adj_phase)
-                    
-    return tide + station["offset"]
-
-
 def predict_tide(t, constituents, station):
     """Predict the tide at a given time t, using the constituents
     and station data"""
@@ -145,11 +74,12 @@ def cli(input_file, station, min_amplitude=1e-3):
             break 
     
     if target_station is not None:
-        for i in range(1):
+        #for i in range(1):
             # get time at start of the hour
-            gm_t = time.gmtime(time.time())
-            t = time.mktime((gm_t[0], gm_t[1], gm_t[2], gm_t[3]+i, 0, 0, 0, 0, 0))
+            #gm_t = time.gmtime(time.time())
+            #t = time.mktime((gm_t[0], gm_t[1], gm_t[2], gm_t[3]+i, 0, 0, 0, 0, 0))
             #print(target_station["name"], time.asctime(time.gmtime(t)), t, predict_tide(t, constituents, station))
+            t = time.time() 
             print(t, f"{predict_tide(t, constituents, station):.6f}")
     return all_stations 
     
